81846247a3903cf55d513ffa3c06288abc48ae85
giusdp <dpl.gsp@tutanota.com>
2022-08-27 14:27:34 +0200

    feat(admin-deploy): reset sub command to remove local deployment

--- pkg/command/admin.go
+++ pkg/command/admin.go
@@ -75,3 +75,36 @@ func (d *deploy) Run(ctx context.Context, logger log.FLogger) error {
 
 	return err
 }
+
+func (r *reset) Run(ctx context.Context, logger log.FLogger) error {
+	logger.Info("Removing local funless deployment...\n")
+
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithVersion("1.41"))
+	if err != nil {
+		return err
+	}
+
+	logger.StartSpinner("Removing Core container... ☠️")
+	if err := logger.StopSpinner(admin.RemoveFLContainer(ctx, cli, "fl-core")); err != nil {
+		return err
+	}
+
+	logger.StartSpinner("Removing Worker container... 🔪")
+	if err := logger.StopSpinner(admin.RemoveFLContainer(ctx, cli, "fl-worker")); err != nil {
+		return err
+	}
+
+	logger.StartSpinner("Removing the function containers... 🔫")
+	if err := logger.StopSpinner(admin.RemoveFunctionContainers(ctx, cli)); err != nil {
+		return err
+	}
+
+	logger.StartSpinner("Removing fl_net network... ✂️")
+	if err := logger.StopSpinner(admin.RemoveFLNetwork(ctx, cli, "fl_net")); err != nil {
+		return err
+	}
+
+	logger.Info("\nAll clear! 👍")
+
+	return err
+}
